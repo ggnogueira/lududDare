23314be2ce5d9cf60eddc01e32b3843cd06a5ff5
ggnogueirar <ggnogueira@inf.ufes.br>
2018-04-21 22:44:52 -0300

    Assets do Tutorial adicionados

--- A42_LD41_FightingGame/CommonScripts/MainMenu.gd
+++ A42_LD41_FightingGame/CommonScripts/MainMenu.gd
@@ -1,12 +1,6 @@
 extends Node
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
 	pass
 
 func _process(delta):
@@ -14,7 +8,6 @@ func _process(delta):
 
 func _on_NewGame_pressed():
 	get_tree().change_scene("res://Scenes/Main.tscn")
-	#get_tree().change_scene("ress://Scenes/Main.tscn")
 	pass # replace with function body
 
 
